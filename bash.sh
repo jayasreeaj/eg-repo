b8d5c2caadbd204e0007a6a302d2a6761bc6fbfb
jayasreeaj <83941193+jayasreeaj@users.noreply.github.com>
2022-09-14 12:45:23 +0530

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -5,9 +5,12 @@
 # Define bash shell variable called var 
 # Avoid spaces around the assignment operator (=)
 var="Hello World"
+var1="how r u"
  
 # print it 
 echo "$var"
+echo "$var2"
  
 # Another way of printing it
 printf "%s\n" "$var"
+printf "%s\n" "$var2"
